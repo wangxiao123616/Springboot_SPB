a844541286aa836f9767bb183481f34f8b1ae174
xiaowang <3197966175@qq.com>
2022-03-19 23:27:32 +0800

    修改代码

--- src/main/java/com/soft/spb/controller/AppVersionController.java
+++ src/main/java/com/soft/spb/controller/AppVersionController.java
@@ -4,6 +4,7 @@ package com.soft.spb.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
--- src/main/java/com/soft/spb/service/FollowService.java
+++ src/main/java/com/soft/spb/service/FollowService.java
@@ -13,4 +13,4 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface FollowService extends IService<Follow> {
 
-}
+  }
