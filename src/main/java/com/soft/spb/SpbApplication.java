3b0be5a4dd0f8364298bdab80ae168b88c5957d0
xiaowang <3197966175@qq.com>
2022-03-19 19:09:53 +0800

    初始化仓库

---
+++ src/main/java/com/soft/spb/SpbApplication.java
@@ -0,0 +1,13 @@
+package com.soft.spb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpbApplication.class, args);
+    }
+
+}
